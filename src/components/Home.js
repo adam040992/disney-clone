d2ce2c49326b76befcae968d5393360aad87172b
adam040992 <adam040992@gmail.com>
2021-05-11 18:27:28 +0200

    ADD: Carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
 
 function Home() {
     return (
         <Container>
-            Home
+            <ImgSlider />
         </Container>
     )
 }
@@ -14,4 +15,16 @@ export default Home
 const Container = styled.div`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    position: relative;
+
+    &:before {
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
 `;
