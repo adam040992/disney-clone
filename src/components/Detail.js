fe0832d1e1f11428bbd8f35d55b5cd1b85994f7a
adam040992 <adam040992@gmail.com>
2021-05-19 18:23:47 +0200

    Add: Firebase

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -65,6 +65,7 @@ const ImageTitle = styled.div`
     min-width: 200px;
     height: 30vh;
     width: 35vh;
+    margin-top: 60px;
 
     img {
         height: 100%;
@@ -137,4 +138,5 @@ const Description = styled.div`
     font-size: 20px;
     margin-top: 16px;
     color: rgb(249, 249, 249);
+    max-width: 600px;
 `;
