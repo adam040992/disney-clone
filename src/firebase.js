41750ede51b2578a626a82ad2ba770df213b8f45
adam040992 <adam040992@gmail.com>
2023-02-16 22:44:39 +0100

    Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/database';
+import 'firebase/storage';
+import 'firebase/auth';
+import 'firebase/firestore';
 
   const firebaseConfig = {
     apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
