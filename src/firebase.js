17263c14c3e44446e7d2b49f2a57d288040ceab0
adam040992 <adam040992@gmail.com>
2023-02-17 15:59:49 +0100

    Firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,23 @@ import 'firebase/storage';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+  //   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+  //   projectId: "disneyplus-clone-a33d5",
+  //   storageBucket: "disneyplus-clone-a33d5.appspot.com",
+  //   messagingSenderId: "37918794208",
+  //   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+  //   measurementId: "G-DRVLJKWRWG",
+  // };
+
   const firebaseConfig = {
-    apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-    authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-    projectId: "disneyplus-clone-a33d5",
-    storageBucket: "disneyplus-clone-a33d5.appspot.com",
-    messagingSenderId: "37918794208",
-    appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-    measurementId: "G-DRVLJKWRWG",
+    apiKey: "AIzaSyAkBh5GKWBkoK_fYDBB06bgbULiZhN_SIk",
+    authDomain: "disney-clone-5b451.firebaseapp.com",
+    projectId: "disney-clone-5b451",
+    storageBucket: "disney-clone-5b451.appspot.com",
+    messagingSenderId: "699227743631",
+    appId: "1:699227743631:web:10894c7a907e4f9d0117fa"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
